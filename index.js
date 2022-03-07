2c932b9315f8277cd42ea1b777e7b867668d163f
Glitch (github-profile-custom-widget) <none>
2022-03-07 02:34:41 +0000

    Import of Alex5ander/github-profile-custom-widget

--- index.js
+++ index.js
@@ -60,20 +60,20 @@ app.get('/', async (req, res) => {
             'cache-control': 'max-age=0, no-cache, no-store, must-revalidate'
         });
         res.send(`
-            <svg version="1.1" style="background-color:#fff" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  viewBox="0 0 300 100" width="18.75rem" height="5.5rem">
+            <svg version="1.1" style="background-color:#fff" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  viewBox="0 0 300 128" width="18.75rem" height="8rem">
                 <text x="50%" y="2rem" dominant-baseline="middle" font-size="2rem" text-anchor="middle" >
                     ${current.temp_c} º
                 </text>
 
-                <text x="50%" y="3.5rem" dominant-baseline="middle" font-size="1rem" text-anchor="middle" >
+                <text x="50%" y="4rem" dominant-baseline="middle" font-size="1rem" text-anchor="middle" >
                     ${condition.text}\n
                 </text>
 
-                <text x="50%" y="4.5rem" dominant-baseline="middle" font-size="1rem" text-anchor="middle" >
+                <text x="50%" y="6rem" dominant-baseline="middle" font-size="1rem" text-anchor="middle" >
                     ${location.name} ${location.region}, ${location.country}.
                 </text>
 
-                <text x="50%" y="5.5rem" dominant-baseline="middle" font-size="1rem" text-anchor="middle" >
+                <text x="50%" y="8rem" dominant-baseline="middle" font-size="1rem" text-anchor="middle" >
                     Total de visitas: ${result} 
                 </text>
             </svg>

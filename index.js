622d8827f7ec6e06f3f9370731dc426be466f894
Glitch (github-profile-custom-widget) <none>
2022-03-07 02:49:59 +0000

    Import of Alex5ander/github-profile-custom-widget

--- index.js
+++ index.js
@@ -63,21 +63,21 @@ app.get('/', async (req, res) => {
             'cache-control': 'max-age=0, no-cache, no-store, must-revalidate'
         });
         res.send(`
-            <svg version="1.1" style="background-color:#abf" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  viewBox="0 0 300 176" width="18.75rem" height="11rem">
+            <svg version="1.1" style="background-color:#abf" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  viewBox="0 0 300 192" width="18.75rem" height="12rem">
                 <image x="7.375rem" xlink:href="data:image/png;base64,${b64}" />
-                <text x="50%" y="4rem" dominant-baseline="middle" font-size="2rem" text-anchor="middle" fill="#fff" >
+                <text x="50%" y="5rem" dominant-baseline="middle" font-size="2rem" text-anchor="middle" fill="#fff" >
                     ${current.temp_c} º
                 </text>
 
-                <text x="50%" y="6rem" dominant-baseline="middle" font-size="1rem" text-anchor="middle" fill="#fff" >
+                <text x="50%" y="7rem" dominant-baseline="middle" font-size="1rem" text-anchor="middle" fill="#fff" >
                     ${condition.text}\n
                 </text>
 
-                <text x="50%" y="8rem" dominant-baseline="middle" font-size="1rem" text-anchor="middle" fill="#fff" >
+                <text x="50%" y="9rem" dominant-baseline="middle" font-size="1rem" text-anchor="middle" fill="#fff" >
                     ${location.name} ${location.region}, ${location.country}.
                 </text>
 
-                <text x="50%" y="10rem" dominant-baseline="middle" font-size="1rem" text-anchor="middle" fill="#fff" >
+                <text x="50%" y="11rem" dominant-baseline="middle" font-size="1rem" text-anchor="middle" fill="#fff" >
                     Total de visitas: ${result} 
                 </text>
             </svg>

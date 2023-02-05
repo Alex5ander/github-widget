a784ea89eb13ca52d4fe3c35cc1e76505639ead7
Alexsander Gutierrez Gonçalves <38378566+Alex5ander@users.noreply.github.com>
2023-02-05 14:49:24 -0300

    no-chache

--- pages/api/visit.ts
+++ pages/api/visit.ts
@@ -85,7 +85,7 @@ export default async function handler(
   res.setHeader('content-type', 'image/svg+xml');
   res.setHeader(
     'cache-control',
-    'max-age=3600'
+    'max-age=3600, no-cache'
   );
   res.send(
     `

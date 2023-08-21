fb3f1ed6e311d50266ee97c2081d2ce11e49f533
Alexsander Gutierrez <alexsandergutierreez@gmail.com>
2023-08-20 23:48:01 -0300

    changes

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useRef } from 'react';
+
+export default function Home() {
+  const ref = useRef<HTMLDivElement>('');
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetch('/api/visit');
+      const svg = await data.text();
+      ref.current.innerHTML = svg;
+    })();
+  }, []);
+
+  return <div ref={ref}></div>;
+}

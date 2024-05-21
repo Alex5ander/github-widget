8715f8db639b0be7463ff104cf51001e0e0061ad
Alexsander Gutierrez <alexsandergutierreez@gmail.com>
2024-05-21 15:08:33 -0300

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,4 @@
-export const getServerSideProps = async () => {
-  const data = await fetch(process.env.URL + '/api/visit');
-  const svg = await data.text();
-  return { props: { svg } };
-};
 
 export default function Home(props: any) {
-  return <div dangerouslySetInnerHTML={{ __html: props.svg }}></div>;
+  return <div ></div>;
 }

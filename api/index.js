6e5ccaa6e0c78ad1f0552492cf657b5e498b7bc8
Alexsander Gutierrez <alexsandergutierreez@gmail.com>
2024-05-21 16:36:06 -0300

    change port

--- api/index.js
+++ api/index.js
@@ -6,6 +6,6 @@ const app = express();
 app.get("/", (_, res) => res.send("Express on Vercel"));
 app.get('/api/visit', visit);
 
-app.listen(80, () => console.log("Server ready on port 3000."));
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 export default app;

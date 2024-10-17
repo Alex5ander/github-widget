0aeed515e57f9875f6bd74c8d66e545c869b5bda
Glitch (github-profile-custom-widget) <none>
2024-10-17 00:35:39 +0000

    👩‍❤️‍💋‍👩🛋 Checkpoint ./api/visit.js:11470847/6705 ./api/index.js:11470847/647 ./package.json:11470847/647 ./src/weather.js:11470847/37

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const visit = require('./visit.js');
-const { config } = require('dotenv');
+const express = require("express");
+const visit = require("./visit.js");
+const { config } = require("dotenv");
 config();
 
 const app = express();
 
 app.get("/", (_, res) => res.send("Express on Vercel"));
-app.get('/api/visit', visit);
+app.get("/api/visit", visit);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 

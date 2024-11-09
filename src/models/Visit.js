08a18ad18792a7b9cb18680c53a8543ad12a2ec1
Glitch (github-profile-custom-widget) <none>
2024-11-08 21:26:37 -0300

    fix

--- src/models/Visit.js
+++ src/models/Visit.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 const Datastore = require("nedb");
 const db = new Datastore({ filename: "./database.db", autoload: true });
 const Visit = () => ({ created_at: new Date().getTime() });
 module.exports = { Visit, db };
-=======
-const Visit = () => ({ created_at: new Date().getTime() });
-module.exports = Visit;
->>>>>>> 8e50249c01a3ba01415d158fb7d46ca3105b3e03
